2fa96e3c40f2acd58936b919d8ebb3ca0a1d9eaf
Biser Svilenov Stanev <biser@MacBook-Air-de-Biser.local>
2024-04-08 19:38:52 +0200

    ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import './App.css';
+import React from 'react';
+import useCustomHook from './hooks/useCustomHook';
 
-function App() {
-  const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
-  const urlRick = 'https://rickandmortyapi.com/api/character/1';
-  
+const App = () => {
+  const { data: pokemonData, isLoading: pokemonLoading, error: pokemonError } = useCustomHook('https://pokeapi.co/api/v2/pokemon/1');
+  const { data: rickData, isLoading: rickLoading, error: rickError } = useCustomHook('https://rickandmortyapi.com/api/character/1');
+
+  if (pokemonLoading || rickLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (pokemonError || rickError) {
+    return <div>Error: {pokemonError || rickError.message}</div>;
+  }
+
   return (
-    <>
-    </>
+    <div>
+      <h2>Personaje Pokemon</h2>
+      {pokemonData && (
+        <>
+          <p>{pokemonData.name}</p>
+          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+        </>
+      )}
+
+      <h2>Personaje Rick and Morty</h2>
+      {rickData && (
+        <>
+          <p>{rickData.name}</p>
+          <img src={rickData.image} alt={rickData.name} />
+        </>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useCustomHook.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const useCustomHook = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+
+    // Cleanup function
+    return () => {
+      // Cleanup code if needed
+    };
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useCustomHook;
